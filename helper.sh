8357a11a9542b883504e84971da6b406f38a3b9e
David Houdek <houdeda2@fit.cvut.cz>
2023-04-07 17:08:01 +0200

    Initial commit

---
+++ helper.sh
@@ -0,0 +1,327 @@
+#!/bin/bash
+
+
+#########################
+######### CONFIG ########
+#########################
+
+source /home/"${SUDO_USER:-${USER}}"/.config/fithelper/config
+version="23.04.07"
+
+#########################
+####### FUNCTIONS #######
+#########################
+
+function mount() {
+    nmcli con up id fit-vpn >/dev/null
+    sudo mount.cifs //drive.fit.cvut.cz/home/"$DriveUser" "$MountDir" -o sec=ntlmv2i,file_mode=0700,dir_mode=0700,uid=$UID,user="$DriveUser",pass="$DrivePass" >/dev/null
+}
+
+function unmount() {
+    sudo umount "$MountDir" >/dev/null
+    nmcli con down id fit-vpn >/dev/null
+}
+
+function cpSap() {
+    cp -ru "$MountDir"/Documents/SAP/* "$LocalDir"/"$SAPSubDir"
+}
+
+function syncGit() {
+    cd "$LocalDir"              || echo "cd to '$LocalDir' failed"; exit 1
+    git add -- *glob*
+    git commit
+    git push
+    cd -                        || echo "IDK how you managed to do that, but cd to previous dir failed"; exit 1
+}
+
+function cPa2() {
+    cp -ru "$LocalDir"/"$PA2SubDir"/* "$FacultyPA2GitDir"
+}
+
+function syncGitPa2() {
+    cd $"$FacultyPA2GitDir"     || echo "cd to '$FacultyPA2GitDir' failed"; exit 1
+    git add -- *glob*
+    git commit
+    git push
+    cd -                        || echo "IDK how you managed to do that, but cd to previous dir failed"; exit 1
+}
+
+function cpPsi() {
+    cp "$PSIIDEDir"/main.py "$LocalDir"/"$PSISubDir"
+}
+
+#########################
+### DISPLAY FUNCTIONS ###
+#########################
+
+function displayWorking() {
+    dialog \
+        --backtitle "FITHELPER GUI" \
+        --title "FITHELPER" \
+        --infobox 'Working...' 6 20
+}
+
+function displayDone() {
+    dialog  \
+        --backtitle "FITHELPER GUI" \
+        --title "FITHELPER" \
+        --msgbox 'Done' 6 20
+}
+
+function displayMounting() {
+    dialog \
+        --backtitle "FITHELPER GUI" \
+        --title "FITHELPER" \
+        --infobox 'Mounting...' 6 20
+}
+
+function displayUnmounting() {
+    dialog \
+        --backtitle "FITHELPER GUI" \
+        --title "FITHELPER" \
+        --infobox 'Copying SAP...' 6 20
+}
+
+function displayCopyingSAP() {
+    dialog \
+        --backtitle "FITHELPER GUI" \
+        --title "FITHELPER" \
+        --infobox 'Copying SAP...' 6 20
+}
+
+function displaySyncingGit() {
+    dialog \
+        --backtitle "FITHELPER GUI" \
+        --title "FITHELPER" \
+        --infobox 'Syncing git...' 6 20
+
+    sleep 1
+}
+
+function displaySyncingFacGit() {
+    dialog \
+        --backtitle "FITHELPER GUI" \
+        --title "FITHELPER" \
+        --infobox 'Syncing faculty git...' 6 20
+
+    sleep 1
+}
+
+function displayCopyingPA2() {
+    dialog \
+        --backtitle "FITHELPER GUI" \
+        --title "FITHELPER" \
+        --infobox 'Copying PA2...' 6 20
+}
+
+function displayCopyingPSI() {
+    dialog \
+        --backtitle "FITHELPER GUI" \
+        --title "FITHELPER" \
+        --infobox 'Copying PSI...' 6 20
+}
+
+#########################
+######### GUI ###########
+#########################
+
+function GUI() {
+    menu=(mount "Enable vpn and connect to faculty drive"
+    unmount "Unmount drive and disable vpn"
+    csap "Copy files from SAP to local dir"
+    allsap "Do everything for SAP"
+    syncgit "Commit and push to repo"
+    cpa2 "Copy PA2 folder to faculty repo"
+    syncgitpa2 "Commit and push PA2 to faculty repo"
+    allpa2 "Do both above"
+    psi "Copy PSI semester work to local dir"
+    everything "DANGEROUS! DOES EVERYTHING!" )
+
+    choice=$(
+    dialog --clear --keep-tite \
+    --backtitle "FITHELPER GUI" \
+    --title "FITHELPER" \
+    --menu "      What can i do for you?" \
+    20 60 10 \
+    "${menu[@]}" \
+    2>&1 >/dev/tty
+    )
+
+    if [[ -z $choice ]]; then
+      echo 'No input...'
+      exit 0
+    fi
+
+    displayWorking
+
+    if [ "$choice" = 'mount' ]; then
+      displayMounting
+      mount
+
+    elif [ "$choice" = 'unmount' ]; then
+      displayUnmounting
+      unmount
+
+    elif [ "$choice" = 'csap' ]; then
+      displayCopyingSAP
+      cpSap
+
+    elif [ "$choice" = 'allsap' ]; then
+      displayMounting
+      mount
+      displayCopyingSAP
+      cpSap
+      displayUnmounting
+      unmount
+
+    elif [ "$choice" = 'syncgit' ]; then
+      displaySyncingGit
+      syncGit
+
+    elif [ "$choice" = 'cpa2' ]; then
+      displayCopyingPA2
+      cPa2
+
+    elif [ "$choice" = 'syncgitpa2' ]; then
+      displaySyncingFacGit
+      syncGitPa2
+
+    elif [ "$choice" = 'allpa2' ]; then
+      displayCopyingPA2
+      cPa2
+      displaySyncingFacGit
+      syncGitPa2
+
+    elif [ "$choice" = 'psi' ]; then
+      displayCopyingPSI
+      cpPsi
+
+    elif [ "$choice" = 'everything' ]; then
+      displayMounting
+      mount
+      displayCopyingSAP
+      cpSap
+      displayUnmounting
+      cpPsi
+      displayCopyingPA2
+      cPa2
+      displaySyncingGit
+      syncGit
+      displaySyncingFacGit
+      syncGitPa2
+      displayUnmounting
+      unmount
+    fi
+
+    displayDone
+}
+
+#########################
+######### MAIN ##########
+#########################
+
+if [ "$1" = 'mount' ]; then
+    echo 'Mounting...'
+    mount
+    echo 'Done'
+
+elif [ "$1" = 'unmount' ]; then
+    echo 'Unmounting...'
+    unmount
+    echo 'Done'
+
+elif [ "$1" = 'csap' ]; then
+    echo 'Copying SAP...'
+    cpSap
+    echo 'Done'
+
+elif [ "$1" = 'allsap' ]; then
+    echo 'Mounting...'
+    mount
+    echo 'Copying SAP...'
+    cpSap
+    echo 'Unmounting...'
+    unmount
+    echo 'Done'
+
+elif [ "$1" = 'syncgit' ]; then
+    echo 'Syncing git...'
+    syncGit
+    echo 'Done'
+
+elif [ "$1" = 'cpa2' ]; then
+    echo 'Copying PA2...'
+    cpa2
+    echo 'Done'
+
+elif [ "$1" = 'syncgitpa2' ]; then
+    echo 'Syncing PA2 git...'
+    syncGitPa2
+    echo 'Done'
+
+elif [ "$1" = 'allpa2' ]; then
+    echo 'Copying PA2...'
+    cPa2
+    echo 'Syncing PA2 git...'
+    syncGitPa2
+    echo 'Done'
+
+elif [ "$1" = 'psi' ]; then
+    cpPsi
+
+elif [ "$1" = 'everything' ]; then
+    echo 'Mounting...'
+    mount
+    echo 'Copying SAP...'
+    cpSap
+    echo 'Copying PSI...'
+    cpPsi
+    echo 'Copying PA2...'
+    cPa2
+    echo 'Syncing git...'
+    syncGit
+    echo 'Syncing PA2 git...'
+    syncGitPa2
+    echo 'Unmounting...'
+    unmount
+    echo 'Done'
+
+elif [ "$1" = 'gui' ]; then
+    GUI
+
+elif [ "$1" = 'help' ]; then
+    echo 'Usage: fithelper [OPTION]'
+    echo 'Helper script for CVUT FIT students'
+    echo ''
+    echo 'Options:'
+    echo '  gui               Display GUI'
+    echo '  mount             Enable vpn and connect to faculty drive'
+    echo '  unmount           Unmount drive and disable vpn'
+    echo '  csap              Copy files from SAP to local dir'
+    echo '  allsap            Do everything for SAP'
+    echo '  syncgit           Commit and push to repo'
+    echo '  cpa2              Copy PA2 folder to faculty repo'
+    echo '  syncgitpa2        Commit and push PA2 to faculty repo'
+    echo '  allpa2            Do both above'
+    echo '  psi               Copy PSI semester work to local dir'
+    echo '  everything        DANGEROUS! DOES EVERYTHING!'
+    echo '  help              Display this help and exit'
+    echo '  display_config    Display config'
+    echo '  version           Display version'
+    echo ''
+    echo 'If there is any problem, please report it on github: https://github.com/Hikari03/fithelper'
+    echo -e '\033[1;31mBefore doing so, try running "fithelper display_config" and check if your config is correct\033[0m'
+    echo -e '\033[1;31mIf you are using GUI, try CLI commands first, as they display errors in more readable way\033[0m'
+    echo -e '\033[1;32mAlso, if you have any idea for new feature, feel free to open issue'
+
+elif [ "$1" = 'display_config' ]; then
+    cat /home/"${SUDO_USER:-${USER}}"/.config/fithelper/config
+
+elif [ "$1" = 'version' ]; then
+    echo "fithelper $version"
+
+else
+    echo 'Invalid option, type "fithelper help" for more'
+fi
+
+exit 0
---
+++ setup.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+# This is a setup script for my helper.sh script
+# It will install helper.sh to /usr/local/bin
+# and create a config file in /home/(username)/.config/fithelper
+
+# Check if user is root
+if [ "$(id -u)" != "0" ]; then
+    echo -e "\033[1;31mThis script must be run as root" 1>&2
+    exit 1
+fi
+
+# Check if helper.sh is in the same directory as this script
+if [ ! -f helper.sh ]; then
+    echo -e "\033[1;31mhelper.sh not found in the same directory as this script" 1>&2
+    exit 1
+fi
+
+echo "Do you want full installation or just update? (full/update)"
+read -r answer
+
+if [ "$answer" = "update" ]; then
+    echo "Updating..."
+    cp helper.sh /usr/local/bin/fithelper
+    chmod +x /usr/local/bin/fithelper
+    exit 0
+fi
+
+# Install helper.sh
+cp helper.sh /usr/local/bin/fithelper
+
+# Create config file
+mkdir /home/"${SUDO_USER:-${USER}}"/.config/fithelper 2>/dev/null
+touch /home/"${SUDO_USER:-${USER}}"/.config/fithelper/config
+
+# Ask for config values
+echo -e "\033[1;31mYou don't have to enter anything, but parts of script will not work."
+echo -e  "\033[1;31mIf you don't enter values, you can edit the config file in /home/${SUDO_USER:-${USER}}/.config/fithelper/config later, but default behaviour is undefined."
+echo ''
+echo -e "\033[1;35mEnter your CVUT username:\033[0;35m"
+read -r DriveUser
+echo ''
+echo -e "\033[1;35mEnter your CVUT password:\033[0;35m"
+read -r DrivePass
+echo ''
+echo -e "\033[1;35mEnter where your local directory will be (absolute path):\033[0;35m"
+read -r LocalDir
+echo ''
+echo -e "\033[1;35mEnter where your mounted drive will be (absolute path):\033[0;35m"
+read -r MountDir
+echo ''
+echo -e "\033[1;35mEnter your faculty PA2 git directory:\033[0;35m"
+read -r FacultyPA2GitDir
+echo ''
+echo -e "\033[1;35mEnter your SAP directory on faculty drive:\033[0;35m"
+read -r SAPRemoteDir
+echo ''
+echo -e "\033[1;35mEnter your PSI IDE directory:\033[0;35m"
+read -r PSIIDEDir
+
+echo ''
+echo -e "\033[0;35mThere are some default values, but you can change them later in /home/${SUDO_USER:-${USER}}/.config/fithelper/config"
+echo -e "\033[0;35mBy default, the script uses number of semester for subdirectories, but you can change it to something else."
+
+# Write config values to config file
+echo "#########################" > /home/hikari/.config/fithelper/config
+echo "######### CONFIG ########" >> /home/hikari/.config/fithelper/config
+echo "#########################" >> /home/hikari/.config/fithelper/config
+echo "DriveUser=\"$DriveUser\"" >> /home/hikari/.config/fithelper/config
+echo "DrivePass=\"$DrivePass\"" >> /home/hikari/.config/fithelper/config
+echo "LocalDir=\"$LocalDir\"" >> /home/hikari/.config/fithelper/config
+echo "MountDir=\"$MountDir\"" >> /home/hikari/.config/fithelper/config
+echo "FacultyPA2GitDir=\"$FacultyPA2GitDir\"" >> /home/hikari/.config/fithelper/config
+echo "SAPRemoteDir=\"$SAPRemoteDir\"" >> /home/hikari/.config/fithelper/config
+echo "PSIIDEDir=\"$PSIIDEDir\"" >> /home/hikari/.config/fithelper/config
+
+echo '' >> /home/hikari/.config/fithelper/config
+echo "#########################" >> /home/hikari/.config/fithelper/config
+echo "##### DEFAULT VALUES ####" >> /home/hikari/.config/fithelper/config
+echo "#########################" >> /home/hikari/.config/fithelper/config
+echo "PA2SubDir=\"2\.Semestr/PA2\"" >> /home/hikari/.config/fithelper/config
+echo "SAPSubDir=\"2\.Semestr/SAP\"" >> /home/hikari/.config/fithelper/config
+echo "PSISubDir=\"2\.Semestr/PSI\"" >> /home/hikari/.config/fithelper/config
+
+# Create subdirectories
+mkdir "$LocalDir"/2\.Semestr >/dev/null 2>&1
+mkdir "$LocalDir"/2\.Semestr/PA2 >/dev/null 2>&1
+mkdir "$LocalDir"/2\.Semestr/SAP >/dev/null 2>&1
+mkdir "$LocalDir"/2\.Semestr/PSI >/dev/null 2>&1
+
+# Set permissions
+chmod +x /usr/local/bin/fithelper
+chmod +rw /home/"${SUDO_USER:-${USER}}"/.config/fithelper/config
+
+# Done
+echo -e "\033[1;32mSetup complete. You can edit the config file in \033[1;31m/home/${SUDO_USER:-${USER}}/.config/fithelper/config\033[1;32m, if you need to."
+echo -e "\033[1;32mRun fithelper help for more info."
